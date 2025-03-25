3e3cc3899a9abd6f3f221a5d8e5a78e5c20c8652
cheng-chun-yuan <albert2367593@gmail.com>
2025-03-25 15:34:14 +0800

    feat: add doge preview on twitter

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,11 +5,24 @@ import "./globals.css"
 import { UserProvider } from "@/context/user-context"
 
 const inter = Inter({ subsets: ["latin"] })
-
+const NEXT_PUBLIC_BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000"
 export const metadata: Metadata = {
   title: "Personal Website",
   description: "My personal website with message board",
+  openGraph: {
+    siteName: "web-lab",
+    type: "website",
+    title: "title",
+    description: "hihihi"
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "dog",
+    description: "dog is cute",
+    images:`${NEXT_PUBLIC_BASE_URL}/dog.jpg`
+  }
 }
+
 
 export default function RootLayout({
   children,
