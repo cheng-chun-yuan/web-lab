3586644381d9e32575dcc8392c910dbf4805bd3a
cheng-chun-yuan <albert2367593@gmail.com>
2025-03-25 15:58:15 +0800

    feat: add migrate script

---
+++ prisma/migrations/20250325075615_first/migration.sql
@@ -0,0 +1,22 @@
+-- CreateTable
+CREATE TABLE "Message" (
+    "id" SERIAL NOT NULL,
+    "author" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "avatar" TEXT,
+    "userId" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Message_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Visitor" (
+    "id" SERIAL NOT NULL,
+    "count" INTEGER NOT NULL DEFAULT 0,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Visitor_pkey" PRIMARY KEY ("id")
+);
